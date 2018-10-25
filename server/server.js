b06aa9a5d00a41e73fff6edd72bf16a359e39bb8
BhavikJoshi <bhavik@getwalnut.com>
2018-10-25 17:01:57 +0530

    Added Heroku SCripts

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const port = process.env.PORT || 3000;
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
@@ -9,6 +10,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!');
 })
