d0b91616624de2e38a00679279cfb162a8f8f985
BhavikJoshi <bhavik@getwalnut.com>
2018-10-26 02:16:33 +0530

    Added Expense Summary Page

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseFilter from './ExpenseListFilter';
+import ExpenseSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpenseSummary />
     <ExpenseFilter />
     <ExpenseList />
   </div>
